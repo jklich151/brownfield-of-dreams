8c1f1dffa1964782e1521a81c5b845215e81029d
Jenny Klich <jennyklich@gmail.com>
2020-04-02 06:47:23 -0600

    Add finished delete videos when tutorials are deleted.

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -17,4 +17,21 @@ feature "An admin can delete a tutorial" do
 
     expect(page).to have_css('.admin-tutorial-card', count: 1)
   end
+
+  scenario "videos are deleted when tutorial is deleted" do
+    admin = create(:admin)
+    tutorial_1 = create(:tutorial)
+    video_1 = create(:video, tutorial_id: tutorial_1.id)
+    video_2 = create(:video, tutorial_id: tutorial_1.id)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit "/admin/dashboard"
+
+    within(first('.admin-tutorial-card')) do
+      click_link 'Delete'
+    end
+
+    expect(Video.where(id: video_1.id)).to eq([])
+  end
 end
